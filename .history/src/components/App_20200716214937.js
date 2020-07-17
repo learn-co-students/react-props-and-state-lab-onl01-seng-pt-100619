c12f879f947cfa70c470094a5d08ccd29264cff0
steelerx155 <steelerx155@gmail.com>
2020-07-16 22:12:49 -0400

    Done.

---
+++ .history/src/components/App_20200716214937.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+import Filters from './Filters'
+import PetBrowser from './PetBrowser'
+
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      pets: [],
+      filters: {
+        type: 'all',
+      }
+    }
+  }
+
+  fetchPets = () => {
+    let endpoint = '/api/pets';
+
+
+
+
+
+  }
+
+  render() {
+    return (
+      <div className="ui container">
+        <header>
+          <h1 className="ui dividing header">React Animal Shelter</h1>
+        </header>
+        <div className="ui container">
+          <div className="ui grid">
+            <div className="four wide column">
+              <Filters />
+            </div>
+            <div className="twelve wide column">
+              <PetBrowser />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
